c5aa3bcb7cc2443f96cc1e6d9cfcfb7d4f8be9e3
valeryzhitnikov <valery.zhitnikov@legalbet.com>
2025-07-29 18:17:03 +0300

    Fix build errors

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,8 +4,11 @@ const createDelay = (delay: number) => {
 
 export const loginUser = async (email: string, password: string): Promise<void> => {
   await createDelay(3000);
+  console.log(email);
+  console.log(password);
 };
 
 export const resetPassword = async (email: string): Promise<void> => {
   await createDelay(1500);
+  console.log(email);
 };
